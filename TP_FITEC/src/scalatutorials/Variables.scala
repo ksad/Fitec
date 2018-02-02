50ed966a1439433429f3b9ca00045758684e8b97
Karim SAD <ksad.karim@gmail.com>
2018-02-02 23:36:28 +0100

    Add new example of variables declarations

---
+++ TP_FITEC/src/scalatutorials/Variables.scala
@@ -0,0 +1,14 @@
+package scalatutorials
+
+object Variables {
+      def main (args:Array[String]){
+        var x: Int = 16 // For best practice we use this syntax.
+        println(x)
+        
+        var y = 1 + 2 //> x = 3  
+        println(y)
+        y = 3 * 4 //> x = 12  
+        println(y)
+        
+    }
+}
