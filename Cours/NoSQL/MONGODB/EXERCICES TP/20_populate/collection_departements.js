d5545c4f406d9b7b7c8b1958a37d754665a20117
Karim SAD <ksad.karim@gmail.com>
2018-03-09 08:21:56 +0100

    Import sources from Dev-Tools

---
+++ Cours/NoSQL/MONGODB/EXERCICES TP/20_populate/collection_departements.js
@@ -0,0 +1,606 @@
+db.departements.insert({
+   _id: 1,
+   _id_region: 22,
+   code: "01",
+   nom: "Ain"
+});
+db.departements.insert({
+   _id: 2,
+   _id_region: 19,
+   code: "02",
+   nom: "Aisne"
+});
+db.departements.insert({
+   _id: 3,
+   _id_region: 2,
+   code: "03",
+   nom: "Allier"
+});
+db.departements.insert({
+   _id: 4,
+   _id_region: 21,
+   code: "04",
+   nom: "Alpes de Haute Provence"
+});
+db.departements.insert({
+   _id: 5,
+   _id_region: 21,
+   code: "05",
+   nom: "Hautes Alpes"
+});
+db.departements.insert({
+   _id: 6,
+   _id_region: 21,
+   code: "06",
+   nom: "Alpes Maritimes"
+});
+db.departements.insert({
+   _id: 7,
+   _id_region: 22,
+   code: "07",
+   nom: "Ardèche"
+});
+db.departements.insert({
+   _id: 8,
+   _id_region: 6,
+   code: "08",
+   nom: "Ardennes"
+});
+db.departements.insert({
+   _id: 9,
+   _id_region: 14,
+   code: "09",
+   nom: "Ariège"
+});
+db.departements.insert({
+   _id: 10,
+   _id_region: 6,
+   code: "10",
+   nom: "Aube"
+});
+db.departements.insert({
+   _id: 11,
+   _id_region: 11,
+   code: "11",
+   nom: "Aude"
+});
+db.departements.insert({
+   _id: 12,
+   _id_region: 14,
+   code: "12",
+   nom: "Aveyron"
+});
+db.departements.insert({
+   _id: 13,
+   _id_region: 21,
+   code: "13",
+   nom: "Bouches du Rhône"
+});
+db.departements.insert({
+   _id: 14,
+   _id_region: 24,
+   code: "14",
+   nom: "Calvados"
+});
+db.departements.insert({
+   _id: 15,
+   _id_region: 2,
+   code: "15",
+   nom: "Cantal"
+});
+db.departements.insert({
+   _id: 16,
+   _id_region: 20,
+   code: "16",
+   nom: "Charente"
+});
+db.departements.insert({
+   _id: 17,
+   _id_region: 20,
+   code: "17",
+   nom: "Charente Maritime"
+});
+db.departements.insert({
+   _id: 18,
+   _id_region: 5,
+   code: "18",
+   nom: "Cher"
+});
+db.departements.insert({
+   _id: 19,
+   _id_region: 12,
+   code: "19",
+   nom: "Corrèze"
+});
+db.departements.insert({
+   _id: 21,
+   _id_region: 12,
+   code: "23",
+   nom: "Creuse"
+});
+db.departements.insert({
+   _id: 22,
+   _id_region: 1,
+   code: "24",
+   nom: "Dordogne"
+});
+db.departements.insert({
+   _id: 23,
+   _id_region: 9,
+   code: "25",
+   nom: "Doubs"
+});
+db.departements.insert({
+   _id: 24,
+   _id_region: 22,
+   code: "26",
+   nom: "Drôme"
+});
+db.departements.insert({
+   _id: 25,
+   _id_region: 17,
+   code: "27",
+   nom: "Eure"
+});
+db.departements.insert({
+   _id: 26,
+   _id_region: 5,
+   code: "28",
+   nom: "Eure et Loir"
+});
+db.departements.insert({
+   _id: 27,
+   _id_region: 4,
+   code: "29",
+   nom: "Finistère"
+});
+db.departements.insert({
+   _id: 28,
+   _id_region: 11,
+   code: "30",
+   nom: "Gard"
+});
+db.departements.insert({
+   _id: 29,
+   _id_region: 14,
+   code: "31",
+   nom: "Haute Garonne"
+});
+db.departements.insert({
+   _id: 30,
+   _id_region: 14,
+   code: "32",
+   nom: "Gers"
+});
+db.departements.insert({
+   _id: 31,
+   _id_region: 1,
+   code: "33",
+   nom: "Gironde"
+});
+db.departements.insert({
+   _id: 32,
+   _id_region: 11,
+   code: "34",
+   nom: "Hérault"
+});
+db.departements.insert({
+   _id: 33,
+   _id_region: 4,
+   code: "35",
+   nom: "Ille et Vilaine"
+});
+db.departements.insert({
+   _id: 34,
+   _id_region: 5,
+   code: "36",
+   nom: "Indre"
+});
+db.departements.insert({
+   _id: 35,
+   _id_region: 5,
+   code: "37",
+   nom: "Indre et Loire"
+});
+db.departements.insert({
+   _id: 36,
+   _id_region: 22,
+   code: "38",
+   nom: "Isère"
+});
+db.departements.insert({
+   _id: 37,
+   _id_region: 9,
+   code: "39",
+   nom: "Jura"
+});
+db.departements.insert({
+   _id: 38,
+   _id_region: 1,
+   code: "40",
+   nom: "Landes"
+});
+db.departements.insert({
+   _id: 39,
+   _id_region: 5,
+   code: "41",
+   nom: "Loir et Cher"
+});
+db.departements.insert({
+   _id: 40,
+   _id_region: 22,
+   code: "42",
+   nom: "Loire"
+});
+db.departements.insert({
+   _id: 41,
+   _id_region: 2,
+   code: "43",
+   nom: "Haute Loire"
+});
+db.departements.insert({
+   _id: 42,
+   _id_region: 18,
+   code: "44",
+   nom: "Loire Atlantique"
+});
+db.departements.insert({
+   _id: 43,
+   _id_region: 5,
+   code: "45",
+   nom: "Loiret"
+});
+db.departements.insert({
+   _id: 44,
+   _id_region: 14,
+   code: "46",
+   nom: "Lot"
+});
+db.departements.insert({
+   _id: 45,
+   _id_region: 1,
+   code: "47",
+   nom: "Lot et Garonne"
+});
+db.departements.insert({
+   _id: 46,
+   _id_region: 11,
+   code: "48",
+   nom: "Lozère"
+});
+db.departements.insert({
+   _id: 47,
+   _id_region: 18,
+   code: "49",
+   nom: "Maine et Loire"
+});
+db.departements.insert({
+   _id: 48,
+   _id_region: 24,
+   code: "50",
+   nom: "Manche"
+});
+db.departements.insert({
+   _id: 49,
+   _id_region: 6,
+   code: "51",
+   nom: "Marne"
+});
+db.departements.insert({
+   _id: 50,
+   _id_region: 6,
+   code: "52",
+   nom: "Haute Marne"
+});
+db.departements.insert({
+   _id: 51,
+   _id_region: 18,
+   code: "53",
+   nom: "Mayenne"
+});
+db.departements.insert({
+   _id: 52,
+   _id_region: 13,
+   code: "54",
+   nom: "Meurthe et Moselle"
+});
+db.departements.insert({
+   _id: 53,
+   _id_region: 13,
+   code: "55",
+   nom: "Meuse"
+});
+db.departements.insert({
+   _id: 54,
+   _id_region: 4,
+   code: "56",
+   nom: "Morbihan"
+});
+db.departements.insert({
+   _id: 55,
+   _id_region: 13,
+   code: "57",
+   nom: "Moselle"
+});
+db.departements.insert({
+   _id: 56,
+   _id_region: 3,
+   code: "58",
+   nom: "Nièvre"
+});
+db.departements.insert({
+   _id: 57,
+   _id_region: 15,
+   code: "59",
+   nom: "Nord"
+});
+db.departements.insert({
+   _id: 58,
+   _id_region: 19,
+   code: "60",
+   nom: "Oise"
+});
+db.departements.insert({
+   _id: 59,
+   _id_region: 24,
+   code: "61",
+   nom: "Orne"
+});
+db.departements.insert({
+   _id: 60,
+   _id_region: 15,
+   code: "62",
+   nom: "Pas de Calais"
+});
+db.departements.insert({
+   _id: 61,
+   _id_region: 2,
+   code: "63",
+   nom: "Puy de Dôme"
+});
+db.departements.insert({
+   _id: 65,
+   _id_region: 23,
+   code: "67",
+   nom: "Bas Rhin"
+});
+db.departements.insert({
+   _id: 62,
+   _id_region: 1,
+   code: "64",
+   nom: "Pyrénées Atlantiques"
+});
+db.departements.insert({
+   _id: 63,
+   _id_region: 14,
+   code: "65",
+   nom: "Hautes Pyrénées"
+});
+db.departements.insert({
+   _id: 64,
+   _id_region: 11,
+   code: "66",
+   nom: "Pyrénées Orientales"
+});
+db.departements.insert({
+   _id: 66,
+   _id_region: 23,
+   code: "68",
+   nom: "Haut Rhin"
+});
+db.departements.insert({
+   _id: 67,
+   _id_region: 22,
+   code: "69",
+   nom: "Rhône"
+});
+db.departements.insert({
+   _id: 68,
+   _id_region: 9,
+   code: "70",
+   nom: "Haute Saône"
+});
+db.departements.insert({
+   _id: 69,
+   _id_region: 3,
+   code: "71",
+   nom: "Saône et Loire"
+});
+db.departements.insert({
+   _id: 70,
+   _id_region: 18,
+   code: "72",
+   nom: "Sarthe"
+});
+db.departements.insert({
+   _id: 71,
+   _id_region: 22,
+   code: "73",
+   nom: "Savoie"
+});
+db.departements.insert({
+   _id: 72,
+   _id_region: 22,
+   code: "74",
+   nom: "Haute Savoie"
+});
+db.departements.insert({
+   _id: 73,
+   _id_region: 10,
+   code: "75",
+   nom: "Seine"
+});
+db.departements.insert({
+   _id: 74,
+   _id_region: 17,
+   code: "76",
+   nom: "Seine Maritime"
+});
+db.departements.insert({
+   _id: 75,
+   _id_region: 10,
+   code: "77",
+   nom: "Seine et Marne"
+});
+db.departements.insert({
+   _id: 76,
+   _id_region: 10,
+   code: "78",
+   nom: "Yvelines"
+});
+db.departements.insert({
+   _id: 77,
+   _id_region: 20,
+   code: "79",
+   nom: "Deux Sèvres"
+});
+db.departements.insert({
+   _id: 78,
+   _id_region: 19,
+   code: "80",
+   nom: "Somme"
+});
+db.departements.insert({
+   _id: 79,
+   _id_region: 14,
+   code: "81",
+   nom: "Tarn"
+});
+db.departements.insert({
+   _id: 80,
+   _id_region: 14,
+   code: "82",
+   nom: "Tarn et Garonne"
+});
+db.departements.insert({
+   _id: 81,
+   _id_region: 21,
+   code: "83",
+   nom: "Var"
+});
+db.departements.insert({
+   _id: 82,
+   _id_region: 21,
+   code: "84",
+   nom: "Vaucluse"
+});
+db.departements.insert({
+   _id: 83,
+   _id_region: 18,
+   code: "85",
+   nom: "Vendée"
+});
+db.departements.insert({
+   _id: 84,
+   _id_region: 20,
+   code: "86",
+   nom: "Vienne"
+});
+db.departements.insert({
+   _id: 85,
+   _id_region: 12,
+   code: "87",
+   nom: "Haute Vienne"
+});
+db.departements.insert({
+   _id: 86,
+   _id_region: 13,
+   code: "88",
+   nom: "Vosges"
+});
+db.departements.insert({
+   _id: 87,
+   _id_region: 3,
+   code: "89",
+   nom: "Yonne"
+});
+db.departements.insert({
+   _id: 88,
+   _id_region: 9,
+   code: "90",
+   nom: "Territoire de Belfort"
+});
+db.departements.insert({
+   _id: 89,
+   _id_region: 10,
+   code: "91",
+   nom: "Essonne"
+});
+db.departements.insert({
+   _id: 90,
+   _id_region: 10,
+   code: "92",
+   nom: "Hauts de Seine"
+});
+db.departements.insert({
+   _id: 91,
+   _id_region: 10,
+   code: "93",
+   nom: "Seine Saint Denis"
+});
+db.departements.insert({
+   _id: 92,
+   _id_region: 10,
+   code: "94",
+   nom: "Val de Marne"
+});
+db.departements.insert({
+   _id: 93,
+   _id_region: 8,
+   code: "976",
+   nom: "Mayotte"
+});
+db.departements.insert({
+   _id: 94,
+   _id_region: 8,
+   code: "971",
+   nom: "Guadeloupe"
+});
+db.departements.insert({
+   _id: 95,
+   _id_region: 8,
+   code: "973",
+   nom: "Guyane"
+});
+db.departements.insert({
+   _id: 96,
+   _id_region: 8,
+   code: "972",
+   nom: "Martinique"
+});
+db.departements.insert({
+   _id: 97,
+   _id_region: 8,
+   code: "974",
+   nom: "Réunion"
+});
+db.departements.insert({
+   _id: 98,
+   _id_region: 3,
+   code: "21",
+   nom: "Côte d'Or"
+});
+db.departements.insert({
+   _id: 102,
+   _id_region: 7,
+   code: "2A",
+   nom: "Corse du sud"
+});
+db.departements.insert({
+   _id: 100,
+   _id_region: 4,
+   code: "22",
+   nom: "Côtes d'Armor"
+});
+db.departements.insert({
+   _id: 103,
+   _id_region: 7,
+   code: "2B",
+   nom: "Haute Corse"
+});
+db.departements.insert({
+   _id: 104,
+   _id_region: 10,
+   code: "95",
+   nom: "Val d'Oise"
+});
---
+++ Cours/NoSQL/MONGODB/EXERCICES TP/20_populate/collection_regions.js
@@ -0,0 +1,92 @@
+db.regions.insert({
+   _id: 1,
+   nom: "Aquitaine"
+});
+db.regions.insert({
+   _id: 2,
+   nom: "Auvergne"
+});
+db.regions.insert({
+   _id: 3,
+   nom: "Bourgogne"
+});
+db.regions.insert({
+   _id: 4,
+   nom: "Bretagne"
+});
+db.regions.insert({
+   _id: 5,
+   nom: "Centre"
+});
+db.regions.insert({
+   _id: 6,
+   nom: "Champagne Ardenne"
+});
+db.regions.insert({
+   _id: 7,
+   nom: "Corse"
+});
+db.regions.insert({
+   _id: 8,
+   nom: "DOM/TOM"
+});
+db.regions.insert({
+   _id: 9,
+   nom: "Franche Comté"
+});
+db.regions.insert({
+   _id: 10,
+   nom: "Ile de France"
+});
+db.regions.insert({
+   _id: 11,
+   nom: "Languedoc Roussillon"
+});
+db.regions.insert({
+   _id: 12,
+   nom: "Limousin"
+});
+db.regions.insert({
+   _id: 13,
+   nom: "Lorraine"
+});
+db.regions.insert({
+   _id: 14,
+   nom: "Midi Pyrénées"
+});
+db.regions.insert({
+   _id: 15,
+   nom: "Nord Pas de Calais"
+});
+db.regions.insert({
+   _id: 17,
+   nom: "Haute Normandie"
+});
+db.regions.insert({
+   _id: 18,
+   nom: "Pays de la Loire"
+});
+db.regions.insert({
+   _id: 19,
+   nom: "Picardie"
+});
+db.regions.insert({
+   _id: 20,
+   nom: "Poitou Charentes"
+});
+db.regions.insert({
+   _id: 21,
+   nom: "Provence Alpes Côte d'azur"
+});
+db.regions.insert({
+   _id: 22,
+   nom: "Rhône Alpes"
+});
+db.regions.insert({
+   _id: 23,
+   nom: "Alsace"
+});
+db.regions.insert({
+   _id: 24,
+   nom: "Basse-Normandie"
+});
